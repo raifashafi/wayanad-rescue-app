a252d1529bfd4921a93b54e4d06cb2f058efb9a5
raifashafi <raifafisat@gmail.com>
2024-08-11 16:39:41 +0530

    created

--- app.js
+++ app.js
@@ -1,23 +1,51 @@
-const mongoose=require("mongoose")
-const cors=require("cors")
-const express=require("express")
-const bcrypt=require("bcrypt")
-const jwt=require("jsonwebtoken")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const express = require("express")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 const loginModel = require("./models/admin")
-const app=express()
+const app = express()
 app.use(cors())
 app.use(express.json())
+mongoose.connect("mongodb+srv://raifashafi:raifashafi@cluster0.tznb7.mongodb.net/rescueDB?retryWrites=true&w=majority&appName=Cluster0")
 
-app.post("/adminSignup",(req,res)=>{
-    let input=req.body
-    let hashedpassword=bcrypt.hashSync(input.password,10)
-    input.password=hashedpassword
+app.post("/adminSignup", (req, res) => {
+    let input = req.body
+    let hashedpassword = bcrypt.hashSync(input.password, 10)
+    input.password = hashedpassword
     console.log(input)
-    let result=new loginModel(input)
+    let result = new loginModel(input)
     result.save()
-    res.json({"status":"success"})
-    
+    res.json({ "status": "success" })
+
 })
-app.listen(3030,()=>{
+// app.post("/adminSignin", (req, res) => {
+//     let input = req.body
+//     let result = loginModel.find({ username: input.username }).then(
+//         (response) => {
+//             if (response.length > 0) {
+//                 const validator = bcrypt.compareSync(input.password, response[0].password)
+//                 if (validator) {
+//                     jwt.sign({ email: input.username }, "rescue-app", { expiresIn: "3d" },
+//                         (error, token) => {
+//                             if (error) {
+//                                 res.json({ "status": "Invalid Authentication" })
+//                             } else {
+//                                 res.json({ "status": "success", "token": token })
+//                             }
+//                         }
+//                     )
+//                 } else {
+//                     res.json({ "status": "Invalid Password" })
+//                 }
+//             } else {
+//                 res.json({ "status": "Invalid Username" })
+//             }
+//         }
+//     ).catch()
+// })
+
+
+app.listen(5050, () => {
     console.log("server started")
 })
